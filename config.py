3d59a92e01e347afb15d84f4c50bdfb187c5c9b1
marat <maratgalyavov@gmail.com>
2023-12-25 02:12:38 +0300

    security fixes

--- config.py
+++ config.py
@@ -30,6 +30,7 @@ supported_languages = trans.get_languages()
     PLAN
 ) = range(23)
 
-openai.api_key = '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'
-telegram_key = "6668637502:AAEp-lxUpp2f3XKghLzeDSClw7ALZ6Ll0xY"
+openai.api_key = 'your_key'
+telegram_key = "your_telegram_token"
+
 USER_DATA_FILE = "user_data.json"
